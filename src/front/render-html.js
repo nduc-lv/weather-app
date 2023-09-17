2d4d63083b095e5ab7a6d189e44370fead43b6b6
Vu <longvunguyen2003@gmail.com>
2023-09-17 23:03:41 +0700

    MAKE THE PAGE WORK

--- src/front/render-html.js
+++ src/front/render-html.js
@@ -1,13 +1,15 @@
 // RENDER THE DATA
 function displayData(weatherData){
     const display = document.querySelector('.display-weather');
+    const error = document.querySelector('.error');
     const temp = document.querySelector("#temp");
     const humidity = document.querySelector("#humidity");
     const wind = document.querySelector("#wind");
     const condition = document.querySelector("#condition");
     const location = document.querySelector("#location");
 
-    display.display = "block";
+    error.textContent = '';
+    display.style.display = "block";
     temp.textContent = weatherData.temp;
     humidity.textContent = weatherData.humidity;
     wind.textContent = weatherData.wind;
@@ -15,6 +17,8 @@ function displayData(weatherData){
     location.textContent = weatherData.location;
 }
 function displayError(errorMessage){
+    const display = document.querySelector('.display-weather');
+    display.style.display = "none";
     const error = document.querySelector(".error");
     error.textContent = errorMessage;
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import getWeatherInfo from "./core/weather-api";
 import {displayData, displayError} from "./front/render-html";
 async function display(location){
     const info = await getWeatherInfo(location);
+    console.log(location);
     if (info.hasOwnProperty("error")){
         displayError(info.error.message);
     }
@@ -9,3 +10,14 @@ async function display(location){
         displayData(info);
     }
 }
+function getLocation(){
+    const input = document.querySelector("input");
+    const location = input.value;
+    return location;
+}
+document.querySelector("button").addEventListener("click", (e) => {
+    e.preventDefault();
+    const location = getLocation();
+    display(location);
+    console.log(location);
+})
